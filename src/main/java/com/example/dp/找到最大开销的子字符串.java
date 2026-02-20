19ecefb170cbfae7a919085b7339000fd9fff755
taoxiao <taoxiao@zhilehuo.onaliyun.com>
2026-02-20 21:55:22 +0800

    找到最大开销的子字符串

---
+++ src/main/java/com/example/dp/找到最大开销的子字符串.java
@@ -0,0 +1,33 @@
+package com.example.dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 找到最大开销的子字符串 {
+
+    public static void main(String[] args) {
+        int[] a = new int[] {-1,-1,-1};
+        maximumCostSubstring("abc", "abc", a);
+    }
+
+    public static int maximumCostSubstring(String s, String chars, int[] vals) {
+        Map<Character, Integer> map = new HashMap();
+        int len = chars.length();
+        for (int i = 0; i < len; i++) {
+            map.put(chars.charAt(i), vals[i]);
+        }
+
+        for (int i = 'a'; i <= 'z'; i++) {
+            map.put((char)i, map.getOrDefault((char)i, i - 'a' + 1));
+        }
+
+        int size = s.length();
+        int d = 0, p = Integer.MIN_VALUE;
+        for (int i = 0; i < size; i++) {
+            d = Math.max(d, 0) + map.get(s.charAt(i));
+            p = Math.max(d, p);
+        }
+
+        return p;
+    }
+}

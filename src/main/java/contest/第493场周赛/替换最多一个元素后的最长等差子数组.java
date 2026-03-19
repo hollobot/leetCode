6528a2487c92522a9cab7289fd18cf3442faf850
taoxiao <taoxiao@zhilehuo.onaliyun.com>
2026-03-19 21:35:42 +0800

    替换最多一个元素后的最长等差子数组

--- src/main/java/contest/第493场周赛/替换最多一个元素后的最长等差子数组.java
+++ src/main/java/contest/第493场周赛/替换最多一个元素后的最长等差子数组.java
@@ -9,45 +9,61 @@ public class 替换最多一个元素后的最长等差子数组 {
     }
 
     public static int longestArithmetic(int[] nums) {
+
+        int[] pre = calc(nums, 1);
+        int[] suf = calc(nums, -1);
+
+        int max = 0;
+        for (int n : pre) {
+            max = Math.max(max, n);
+        }
+
         int len = nums.length;
-        int max = 2;
+        if (max >= len) {
+            return len;
+        }
+
         for (int i = 1; i < len - 1; i++) {
-            int sum = nums[i + 1] + nums[i - 1];
-            if (sum % 2 != 0) {
+            int d2 = nums[i + 1] - nums[i - 1];
+            if (d2 % 2 != 0) {
                 continue;
             }
-            int tem = nums[i];
-            nums[i] = sum / 2;
-            max = Math.max(max, dfs(nums, i, nums[i + 1] - nums[i]));
-            nums[i] = tem;
+
+            boolean l = i > 1 && (nums[i - 1] - nums[i - 2]) == d2 / 2;
+            boolean r = i < len - 2 && (nums[i + 2] - nums[i + 1]) == d2 / 2;
+            if (l && r) {
+                max = Math.max(max, pre[i - 1] + 1 + suf[i + 1]);
+            } else if (l) {
+                max = Math.max(max, pre[i - 1] + 2);
+            } else if (r) {
+                max = Math.max(max, 2 + suf[i + 1]);
+            }
         }
 
         return max;
+
     }
 
-    public static int dfs(int[] nums, int index, int d) {
+    public static int[] calc(int[] nums, int k) {
         int len = nums.length;
-        int l = index - 1;
-        int r = index + 1;
-        int count = 1;
-        boolean flag1 = true, flag2 = true;
-        while (flag1 || flag2) {
-            if (l >= 0 && nums[l] + d == nums[l + 1]) {
-                count++;
-                l--;
+        int[] ans = new int[len];
+        int j = 2;
+        if (k == 1) {
+            ans[0] = 1;
+            ans[1] = 2;
+        } else if (k == -1) {
+            ans[len - 1] = 1;
+            ans[len - 2] = 2;
+            j = len - 3;
+        }
+        for (int i = j; i < len && i >= 0; i += k) {
+            if ((nums[i - 2 * k] + nums[i]) == nums[i - k] * 2) {
+                ans[i] = ans[i - k] + 1;
             } else {
-                flag1 = false;
+                ans[i] = 2;
             }
-
-            if (r < len && nums[r - 1] + d == nums[r]) {
-                count++;
-                r++;
-            } else {
-                flag2 = false;
-            }
-
         }
 
-        return count;
+        return ans;
     }
 }

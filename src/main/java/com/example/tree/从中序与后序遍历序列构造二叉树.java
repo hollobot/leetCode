004973c4fba09281964a2b03e294cf1540ee3bf8
taoxiao <taoxiao@zhilehuo.onaliyun.com>
2026-04-23 20:13:01 +0800

    从中序与后序遍历序列构造二叉树

---
+++ src/main/java/com/example/tree/从中序与后序遍历序列构造二叉树.java
@@ -0,0 +1,60 @@
+package com.example.tree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 从中序与后序遍历序列构造二叉树 {
+    private int[] inorder;
+    private int[] postorder;
+    private Map<Integer, Integer> map;
+
+    public static void main(String[] args) {
+        new 从中序与后序遍历序列构造二叉树().buildTree(new int[] {9, 3, 15, 20, 7}, new int[] {9, 15, 7, 20, 3});
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        this.inorder = inorder;
+        this.postorder = postorder;
+        this.map = new HashMap();
+        int len = inorder.length;
+        for (int i = 0; i < len; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return dfs(0, len - 1, 0, len - 1);
+    }
+
+    public TreeNode dfs(int inL, int inR, int postL, int postR) {
+        if (inL > inR) {
+            return null;
+        }
+
+        int inVal = postorder[postR];
+        int index = map.get(inVal);
+        int l = index - inL;
+        TreeNode root = new TreeNode(inVal);
+        root.left = dfs(inL, index - 1, postL, postL + l - 1);
+        root.right = dfs(index + 1, inR, postL + l, postR - 1);
+
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
